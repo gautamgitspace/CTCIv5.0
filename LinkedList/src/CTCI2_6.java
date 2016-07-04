345eecd5362fa5d520aada90dbc3469edae678e6
Abhishek Gautam <agautam2@buffalo.edu>
2016-07-04 01:56:57 -0400

    ctci 2.6 final commit

--- LinkedList/src/CTCI2_6.java
+++ LinkedList/src/CTCI2_6.java
@@ -14,12 +14,11 @@ public class CTCI2_6
             fast = fast.next.next;
             if (slow == fast)
             {
-                System.out.println("HERE WE MEET");
+                System.out.println("HERE WE MEET AT " + slow.data);
                 break;
             }
         }
-
-        // Error check - there is no meeting point, and therefore no loop
+        //Check if no loop exists
         if (fast == null || fast.next == null)
         {
             return null;
@@ -33,8 +32,32 @@ public class CTCI2_6
         }
 
         // At this point slow=fast=starting of the loop
+
         return fast;
     }
 
-   
+    public static void main(String[] args)
+    {
+        int list_length = 100;
+        int k = 45;
+        //Generate List
+        LinkedListNode[] nodes = new LinkedListNode[list_length];
+        for (int i = 0; i < list_length; i++)
+        {
+            nodes[i] = new LinkedListNode(i, null, i > 0 ? nodes[i - 1] : null);
+        }
+
+        //Force loop
+        nodes[list_length - 1].next = nodes[list_length - k];
+
+        LinkedListNode loop = FindBeginning(nodes[0]);
+        if (loop == null)
+        {
+            System.out.println("Lucky to survive. No loop found");
+        }
+        else
+        {
+            System.out.println("Starting point of loop: " + loop.data);
+        }
+    }
 }
