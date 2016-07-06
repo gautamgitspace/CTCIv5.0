8ce4d9dd1226d073c6e7bd08193aa24f16c45b7b
Abhishek Gautam <agautam2@buffalo.edu>
2016-07-06 19:27:10 -0400

    added code for testing 2_7.py

--- LinkedList/src/2_7.py
+++ LinkedList/src/2_7.py
@@ -18,3 +18,13 @@ def isPalindrome(linkedlist):
         else:
             slow = slow.next
     return True
+L1 = randomLinkedList(3, 3, 4)
+print "L2:", L1
+print "Is Palindrome? ", isPalindrome(L1)
+L2 = LinkedList()
+for i in range(1,4):
+    L2.addNode(i)
+for i in range(3, 0, -1):
+    L2.addNode(i)
+print "L3:", L2
+print "Is Palindrome? ", isPalindrome(L2)
