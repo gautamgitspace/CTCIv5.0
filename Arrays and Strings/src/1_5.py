3ffcb6137706e9c9f2d59a05b4c069ba7ae3764f
Abhishek Gautam <agautam2@buffalo.edu>
2016-07-08 21:07:15 -0400

    ctci 1.5 optimized, returns the smallest of the two

--- Arrays and Strings/src/1_5.py
+++ Arrays and Strings/src/1_5.py
@@ -1,8 +1,7 @@
-#runtime for this is O(p + k^2) where p is the size of the original string and k is the number of character sequences.
-#where in 'hello', there are 4 character sequences. This approach is slow.
 input = raw_input('Enter String: ')
 count = 1
 resultantStr = str()
+finalStr = str()
 length = len(input)
 comparator = input[0]
 lst = list()
@@ -15,4 +14,8 @@ for item in lst[1:length+1]:
         resultantStr = resultantStr + comparator + str(count)
         comparator=item
         count=1
-print resultantStr+comparator+str(count)
+finalStr = resultantStr+comparator+str(count)
+if len(finalStr) >= length:
+    print input
+else:
+    print finalStr
