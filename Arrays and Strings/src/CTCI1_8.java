534bf3befa44afece6ad379b3107e99dab309ddf
Abhishek Gautam <agautam2@buffalo.edu>
2016-07-09 15:56:24 -0400

    ctci 1.8 java main for testing

--- Arrays and Strings/src/CTCI1_8.java
+++ Arrays and Strings/src/CTCI1_8.java
@@ -24,6 +24,19 @@ public class CTCI1_8
         }
         return false;
     }
-    
 
-}
+    public static void main(String[] args)
+    {
+
+            String word1 = "razer";
+            String word2 = "rraze";
+            String word3 = "waterbottle";
+            String word4 = "bottlerwate";
+            boolean result = isRotation(word1, word2);
+            boolean result2 = isRotation(word3, word4);
+            System.out.println(word1 + ", " + word2 + ": " + result);
+            System.out.println(word3 + ", " + word4 + ": " + result2);
+        }
+    }
+
+
