5f28b65f3b4e40302c20632ea9b35c7141347a44
Abhishek Gautam <agautam2@buffalo.edu>
2016-07-08 20:36:23 -0400

    ctci 1.5 non efficient solution in python

---
+++ Arrays and Strings/src/1_5.py
@@ -0,0 +1,16 @@
+input = raw_input('Enter String: ')
+count = 1
+resultantStr = str()
+length = len(input)
+comparator = input[0]
+lst = list()
+for alphabet in input:
+    lst.append(alphabet)
+for item in lst[1:length+1]:
+    if item == comparator:
+        count=count+1
+    else:
+        resultantStr = resultantStr + comparator + str(count)
+        comparator=item
+        count=1
+print resultantStr+comparator+str(count)
