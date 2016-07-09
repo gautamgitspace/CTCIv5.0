79b59448d2c1d9fb59c9ae4e46f6491c1881b171
Abhishek Gautam <agautam2@buffalo.edu>
2016-07-08 22:33:32 -0400

    ctci 1.5 optimized - finds compressionCount before compressing

--- Arrays and Strings/src/CTCI1_5.java
+++ Arrays and Strings/src/CTCI1_5.java
@@ -4,7 +4,7 @@ public class CTCI1_5
 {
 	int count=1;
 		
-	public String compression(String str)
+	private String compression(String str)
 	{
 		if (str == null || str.isEmpty())
 		{
@@ -12,34 +12,55 @@ public class CTCI1_5
 		}
 		else
 		{
-		StringBuffer customStr = new StringBuffer();
-		char last = str.charAt(0);
-		for(int i=1;i<str.length();i++)
-		{
-		if (last==str.charAt(i))
-		{
-			count++;
+			int compareSize = countCompression(str);
+			System.out.println("Compression Size: " + compareSize + " Original Size: " + str.length());
+			if (compareSize>=str.length())
+				return str;
+			StringBuffer customStr = new StringBuffer();
+			char last = str.charAt(0);
+				for(int i=1;i<str.length();i++)
+				{
+					if (last==str.charAt(i))
+					{
+						count++;
+					}
+					else
+					{
+						customStr.append(last);		//#1	APPEND CHARACTER TO RESULT STRING
+						customStr.append(count);	//#2	APPEND COUNT TO RESULT STRING
+						last=str.charAt(i);			//#3	UPDATE LAST
+						count=1;					//#4	RESET COUNT
+					}
+				}
+			customStr.append(last);		//ENTERTAINING THE VERY LAST CHARACTER IN THE STRING
+			customStr.append(count);
+			return customStr.toString();
 		}
-		else
-		{
-			customStr.append(last);		//#1	APPEND CHARACTER TO RESULT STRING
-			customStr.append(count);	//#2	APPEND COUNT TO RESULT STRING
-			last=str.charAt(i);			//#3	UPDATE LAST	
-			count=1;					//#4	RESET COUNT
-		}
-		}
-		customStr.append(last);		//APPEND CHARACTER TO RESULT STRING
-		customStr.append(count);
-		
-		
-		String afterCompression=customStr.toString();
-		if(afterCompression.length()>=str.length())
-		{
-			return str;
-		}
-		else
-		return customStr.toString();
 	}
+	private int countCompression(String string)
+	{
+		int stringSize = 0;
+		int count = 1;
+		if (string == null || string.isEmpty())
+		{
+			stringSize = 0;
+		}
+		char last = string.charAt(0);
+		for(int i=1;i<string.length();i++)
+		{
+			if (string.charAt(i) == last)
+			{
+				count++;
+			}
+			else
+			{
+				last = string.charAt(i);
+				stringSize += 1 + String.valueOf(count).length();
+				count = 1;
+			}
+		}
+			stringSize += 1 + String.valueOf(count).length();
+		return  stringSize;
 	}
 	public static void main(String args[])
 	{
@@ -50,7 +71,7 @@ public class CTCI1_5
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			String input=br.readLine();
 			String result=obj.compression(input);
-			System.out.println(result);
+			System.out.println("Resulting String: " + result);
 
 		}
 		catch(IOException e)
