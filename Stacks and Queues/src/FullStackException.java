9f87c2695ba51d1fb7a5371f5237bba6a4d010d7
Abhishek Gautam <agautam2@buffalo.edu>
2016-07-12 19:14:40 -0400

    Exception class

---
+++ Stacks and Queues/src/FullStackException.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Gautam on 7/12/16.
+ */
+public class FullStackException extends Exception
+{
+    public FullStackException()
+    {
+        super();
+    }
+
+    public FullStackException(String message)
+    {
+        super(message);
+    }
+}
