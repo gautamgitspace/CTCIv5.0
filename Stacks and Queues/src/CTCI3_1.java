01ece8881a128cc705a2ebaa4c4194c046780a2c
Abhishek Gautam <agautam2@buffalo.edu>
2016-07-12 18:46:19 -0400

    initial code structure

--- Stacks and Queues/src/CTCI3_1.java
+++ Stacks and Queues/src/CTCI3_1.java
@@ -5,4 +5,34 @@
 public class CTCI3_1
 {
 
+    private void push(int stackNumber, int value)
+    {
+
+    }
+    private void pop(int stackNumber)
+    {
+
+    }
+    private int peek(int stackNumber)
+    {
+
+    }
+    private boolean isEmpty(int stackNumber)
+    {
+
+    }
+    private boolean isFull(int stackNumber)
+    {
+
+    }
+    private int whereInBuffer(int stackNumber)
+    {
+
+    }
+    public static void main(String args[])
+    {
+     CTCI3_1 obj = new CTCI3_1();
+
+    }
+
 }
