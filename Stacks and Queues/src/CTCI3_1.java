853989044142ffd448d999e83a9e029d7a87cfc1
Abhishek Gautam <agautam2@buffalo.edu>
2016-07-12 19:11:57 -0400

    code for pop, push, peek, whereInBuffer, isEmpty and isFull

--- Stacks and Queues/src/CTCI3_1.java
+++ Stacks and Queues/src/CTCI3_1.java
@@ -1,3 +1,5 @@
+import java.util.EmptyStackException;
+
 /**
  * Created by Gautam on 7/9/16.
  * single array to implement three stacks
@@ -5,34 +7,57 @@
 public class CTCI3_1
 {
 
-    private void push(int stackNumber, int value)
+    private static int sizeOfStack = 100;
+    private static int[] buffer = new int[sizeOfStack*3];
+    private static int[] stackPointer = {-1, -1, -1};
+    private static void push(int stackNumber, int value)
     {
-
+        //TODO check if we have space to push?
+        isFull(stackNumber);
+        //TODO increment stack pointer and push
+        stackPointer[stackNumber]++;
+        buffer[whereInBuffer(stackNumber)]=value;
     }
-    private void pop(int stackNumber)
+    private static int pop(int stackNumber)
     {
-
+        //TODO check if stack is empty?
+        if(isEmpty(stackNumber))
+            throw new EmptyStackException();
+        //TODO fetch value and return it, reset index and decrement pointer
+        int value = buffer[whereInBuffer(stackNumber)];
+        buffer[whereInBuffer(stackNumber)]=0;
+        stackPointer[stackNumber]--;
+        return value;
     }
-    private int peek(int stackNumber)
+    private static int peek(int stackNumber)
     {
-
+        if(isEmpty(stackNumber))
+            throw new EmptyStackException();
+        return buffer[whereInBuffer(stackNumber)];
     }
-    private boolean isEmpty(int stackNumber)
+    private static boolean isEmpty(int stackNumber)
     {
-
+        //TODO check if stack is empty
+        return stackPointer[stackNumber] == -1;
     }
-    private boolean isFull(int stackNumber)
+    private static void isFull(int stackNumber)
     {
-
+        try
+        {
+            if (stackPointer[stackNumber] + 1 > sizeOfStack)
+            {
+                throw new FullStackException();
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
-    private int whereInBuffer(int stackNumber)
+    private static int whereInBuffer(int stackNumber)
     {
-
+        return stackNumber * sizeOfStack + stackPointer[stackNumber];
     }
-    public static void main(String args[])
-    {
-     CTCI3_1 obj = new CTCI3_1();
-
-    }
+    
 
 }
