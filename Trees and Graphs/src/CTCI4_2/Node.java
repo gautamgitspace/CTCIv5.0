3e67b850df516cdb7cf8a0e02d96ba3928b591ae
Abhishek Gautam <agautam2@buffalo.edu>
2016-08-17 01:28:53 -0400

    Basic Node class for Graphs

---
+++ Trees and Graphs/src/CTCI4_2/Node.java
@@ -0,0 +1,50 @@
+package CTCI4_2;
+
+/**
+ * Created by Gautam on 8/16/16.
+ */
+public class Node
+{
+    Node adjacenyList[];
+    String vertex;
+    int adjacentNodeCount;
+    Driver.State state;
+
+    /*ADJACENT LENGTH IS THE NEIGHBORING NODE COUNT*/
+    /*A NEW NODE WHEN FORMED, TELLS IT'S NAME AND THE NUMBER OF NEIGHBORS IT WILL HAVE*/
+
+    //CONS
+    public Node(String vertex, int adjacentLength)
+    {
+        this.vertex=vertex;
+        adjacenyList = new Node[adjacentLength];
+        adjacentNodeCount = 0;
+    }
+
+    public void addAdjacent(Node x)
+    {
+        /*If you have N nodes, there are N - 1 directed edges than can lead from it (going to every other node). Therefore, the maximum number of edges is N * (N - 1)*/
+        if (adjacentNodeCount < 30)
+        {
+            adjacenyList[adjacentNodeCount] = x;
+            adjacentNodeCount++;
+        }
+        else
+        {
+            System.out.println("NO MORE ADDITION POSSIBLE");
+        }
+    }
+
+    public Node[] getAdjacencyList()
+    {
+        return adjacenyList;
+    }
+
+    public String getVertexName()
+    {
+        return vertex;
+    }
+
+
+
+}
