c10318ad863b31fefdd2d1b2ce12064bb732db93
Abhishek Gautam <agautam2@buffalo.edu>
2016-08-17 17:19:29 -0400

    Got rid off stupid parsing erros

--- Stacks and Queues/src/CTCI3_6/SortStackASC.java
+++ Stacks and Queues/src/CTCI3_6/SortStackASC.java
@@ -9,36 +9,22 @@ import java.util.Stack;
 
 public class SortStackASC
 {
-<<<<<<< HEAD
+
     private static Stack<Integer> sort(Stack<Integer> toBeSorted)
     {
         Stack<Integer> alreadySorted = new Stack<Integer>();
-        while(!toBeSorted.isEmpty())
+        while (!toBeSorted.isEmpty())
         {
             int tmp = toBeSorted.pop();
-            while(!alreadySorted.isEmpty() && alreadySorted.peek() > tmp)
+            while (!alreadySorted.isEmpty() && alreadySorted.peek() > tmp)
             {
                 toBeSorted.push(alreadySorted.pop());
             }
             alreadySorted.push(tmp);
         }
         return alreadySorted;
-=======
-    private static Stack<Integer> sort(Stack<Integer> s)
-    {
-        Stack<Integer> r = new Stack<Integer>();
-        while(!s.isEmpty())
-        {
-            int tmp = s.pop();
-            while(!r.isEmpty() && r.peek() > tmp)
-            {
-                s.push(r.pop());
-            }
-            r.push(tmp);
-        }
-        return r;
->>>>>>> c2c7015f4831910c5fe7206af3ae73efc1f4cbd9
     }
+
     private static int randomInt(int n)
     {
         return (int) (Math.random() * n);
@@ -59,10 +45,8 @@ public class SortStackASC
         }
 
         stack = sort(stack);
-<<<<<<< HEAD
+
         System.out.println("SORTING USING ONE ADDITIONAL STACK O(n^2)");
-=======
->>>>>>> c2c7015f4831910c5fe7206af3ae73efc1f4cbd9
         for(int j=0; j<10; j++)
         {
             int popped = stack.pop();
