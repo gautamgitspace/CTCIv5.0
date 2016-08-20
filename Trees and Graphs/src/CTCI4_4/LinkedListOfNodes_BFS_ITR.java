42c89aae0d5b776fc45d349c9d5e98dd1046167b
Abhishek Gautam <agautam2@buffalo.edu>
2016-08-20 01:05:11 -0400

    List of Tree Nodes using BFS

---
+++ Trees and Graphs/src/CTCI4_4/LinkedListOfNodes_BFS_ITR.java
@@ -0,0 +1,79 @@
+package CTCI4_4;
+
+import CTCI4_1.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ * Created by Gautam on 8/17/16.
+ */
+public class LinkedListOfNodes_BFS_ITR
+{
+    public static ArrayList<LinkedList<TreeNode>> createLevelLinkedList(TreeNode root) {
+        ArrayList<LinkedList<TreeNode>> result = new ArrayList<LinkedList<TreeNode>>();
+
+        LinkedList<TreeNode> current = new LinkedList<TreeNode>();
+        /* "Visit" the root */
+        if (root != null)
+        {
+            current.add(root);
+        }
+
+        while (current.size() > 0)
+        {
+            result.add(current);
+            System.out.println("CURRENT ADDED TO RESULT");
+            /*GO TO NEXT LEVEL OF DEPTH*/
+            LinkedList<TreeNode> parents = current;
+            current = new LinkedList<TreeNode>();
+            for (TreeNode parent : parents)
+            {
+				/* Visit the children */
+                if (parent.left != null)
+                {
+                    current.add(parent.left);
+                    System.out.println("ADDED LEFT TO CURRENT");
+                }
+                if (parent.right != null)
+                {
+                    current.add(parent.right);
+                    System.out.println("ADDED RIGHT TO CURRENT");
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void printResult(ArrayList<LinkedList<TreeNode>> result)
+    {
+        int depth = 0;
+        for(LinkedList<TreeNode> entry : result)
+        {
+            Iterator<TreeNode> iterator = entry.listIterator();
+            System.out.print("LIST AT DEPTH LEVEL " + "[" + depth + "]: ");
+            while(iterator.hasNext())
+            {
+                System.out.print(" " + ((TreeNode)iterator.next()).data);
+            }
+            System.out.println();
+            depth++;
+        }
+    }
+
+
+    public static void main(String[] args)
+    {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(4);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(3);
+        root.right = new TreeNode(8);
+        root.right.left = new TreeNode(7);
+        root.right.right = new TreeNode(9);
+        ArrayList<LinkedList<TreeNode>> list = createLevelLinkedList(root);
+        printResult(list);
+    }
+}
