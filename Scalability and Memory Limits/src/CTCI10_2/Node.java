0b13ee1091fc691aff70b3f20f52c0fa3833c920
Abhishek Gautam <agautam2@buffalo.edu>
2016-08-22 01:15:12 -0400

    Most Important Node(Person with many friends)

---
+++ Scalability and Memory Limits/src/CTCI10_2/Node.java
@@ -0,0 +1,31 @@
+package CTCI10_2;
+
+/**
+ * Created by Gautam on 8/21/16.
+ */
+public class Node
+{
+    /*TO TRACK THE MOST IMPORTANT NODE TO TRACK A FRIEND WHO HAS A LOT OF FRIENDS
+
+    Node mostImportant = null
+
+    void search(node) {
+        nodeInitial = pick a node
+        if(!mostImportant) {
+            mostImportant = nodeInitial
+        }
+
+        for(nodeInitial:outgoingNodes as node) {
+            node:visits++ // this is how you track in-edges
+
+            if(node.visits > mostImportant.visits)
+                mostImportant = node
+
+            if(node is unvisited) {
+                search(node)
+                node:visited = true
+            }
+        }
+    }
+    */
+}
