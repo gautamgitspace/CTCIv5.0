3dc818da607367cf0e2d74eca167c5b654c02275
Abhishek Gautam <agautam2@buffalo.edu>
2016-10-04 16:09:47 -0400

    CTCI 1_3 using Counter in py

---
+++ Arrays and Strings/src/1_3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#check if one stringe is permutation of other - by counting the number of occurrence of chars in both strings and comparing
+from collections import Counter
+def isPermute(str1, str2):
+    count_a = Counter(str1)
+    count_b = Counter(str2)
+    if count_a == count_b:
+        return True
+    else:
+        return False
+print isPermute("abcd", "aabc")
