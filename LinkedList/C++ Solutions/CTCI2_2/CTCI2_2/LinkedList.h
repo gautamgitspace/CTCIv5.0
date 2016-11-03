5e1497e98630544eb34fc5cbe0d55f35686f174e
Abhishek Gautam <agautam2@buffalo.edu>
2016-11-03 00:23:12 -0400

    kth last node using 2 ptrs

---
+++ LinkedList/C++ Solutions/CTCI2_2/CTCI2_2/LinkedList.h
@@ -0,0 +1,36 @@
+//
+//  LinkedList.h
+//  CTCI2_1
+//
+//  Created by Gautam on 11/2/16.
+//  Copyright © 2016 Gautam. All rights reserved.
+//
+
+#ifndef LinkedList_h
+#define LinkedList_h
+
+
+struct Node
+{
+    int data;
+    Node* next;
+    // bool visited;
+};
+
+class LinkedList
+{
+public:
+    Node* head;
+    LinkedList();
+    ~LinkedList();
+    void display();
+    void insert(int value);
+    void createCycle();
+    int ktoLast(Node* head, int k);
+private:
+    // added this so insertion is O(1) time
+    Node* tail;
+};
+
+
+#endif /* LinkedList_h */
