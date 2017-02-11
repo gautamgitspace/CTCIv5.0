399b9ccafce1d3614895c7bbaca053fa25c1fbc6
Abhishek Gautam <agautam2@buffalo.edu>
2017-02-11 23:03:56 +0530

    tested func : compress using bytes.Buffer and strconv.Itoa()

--- Arrays and Strings/GoLang Solutions/ctci_5.go
+++ Arrays and Strings/GoLang Solutions/ctci_5.go
@@ -1,6 +1,8 @@
 package main
 import (
   "fmt"
+  "bytes"
+  "strconv"
 )
 
 func countCompression(inputString string) (int, int) {
@@ -32,29 +34,34 @@ func compress(inputString string) string {
     fmt.Println("ORIGINAL SIZE:", originalLength)
     fmt.Println("COMPRESSED SIZE:", compareSize)
 
-    if compareSize >= len(inputString) {
+    if compareSize >= originalLength {
       return inputString
     }
-    var customString string = ""
+    //var customString string = ""
+    var buffer bytes.Buffer
     last := string(inputString[0])
-    for i:=0; i<len(inputString); i++ {
+    for i:=1; i<len(inputString); i++ {
       if last == string(inputString[i]) {
         count++
       }else{
-          customString = customString + (string(last))
-          customString = customString + (string(count))
+          buffer.WriteString(string(last))
+          //customString += (string(last))
+          //fmt.Println("CUSTOM STRING:", buffer.String())
+          buffer.WriteString(strconv.Itoa(count))
+          //fmt.Println("CUSTOM STRING AFTER APPENDING COUNT:", buffer.String())
+          //customString += (string(count))
           last = string(inputString[i])
           count = 1
         }
       }
-      customString = customString + string(last)
-      customString = customString + string(count)
-      return customString
+      buffer.WriteString(last)
+      //customString = customString + string(last)
+      buffer.WriteString(strconv.Itoa(count))
+      //customString = customString + string(count)
+      return buffer.String()
   }
 }
 
 func main() {
-  compressedLength, originalLength := countCompression("abc")
-  fmt.Println("COMPRESSED LENGTH:", compressedLength)
-  fmt.Println("ORIGINAL LENGTH:", originalLength)
+  fmt.Println("RESULTANT STRING:", compress("AABBCCCDDEEEEE"))
 }
