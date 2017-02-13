4bca38afdfcecdc5e155821c222a7ac174cac5bb
Abhishek Gautam <agautam2@buffalo.edu>
2017-02-13 20:02:31 +0530

    definitions for func: isSubstring and isRotation

--- Arrays and Strings/GoLang Solutions/ctci_8.go
+++ Arrays and Strings/GoLang Solutions/ctci_8.go
@@ -1,16 +1,27 @@
 package main
 import (
   "fmt"
+  "strings"
 )
 
 func isSubstring(big string, small string) bool {
-
+  if strings.Contains(big, small) {
+    return true
+  }
+    return false
 }
 
 func isRotation(s1 string, s2 string) bool {
-
+  if len(s1) == len(s2) && len(s1) > 0 {
+    bigString := s1 + s2
+    fmt.Println(bigString)
+    return isSubstring(bigString, s2)
+  }
+  return false
 }
 
 func main() {
-  
+first := "razer"
+second := "hello"
+fmt.Println(isRotation(first, second))
 }
