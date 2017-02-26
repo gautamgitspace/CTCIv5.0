e61d28f8256d9351010ae6f726a297e8260ce612
Abhishek Gautam <agautam2@buffalo.edu>
2017-02-26 12:41:57 -0800

    func to print list contents

--- LinkedList/GoLang Solutions/linkedlist/linkedlist.go
+++ LinkedList/GoLang Solutions/linkedlist/linkedlist.go
@@ -1,36 +1,46 @@
 package main
+
 import (
-  "fmt"
-  "math/rand"
-  "time"
-  "strings"
-  "strconv"
-  "bytes"
+	"bytes"
+	"fmt"
 )
 
 type Node struct {
-  next *Node
-  data int
+	next *Node
+	data int
 }
 
 type List struct {
-  head *Node
-  length int
+	head   *Node
+	length int
 }
 
 //func to set next
-func (n *Node) setNext(s *Node){
-  n.next = s
+func (n *Node) setNext(s *Node) {
+	n.next = s
 }
+
 //func to set data
-func (n *Node) setItem(value int){
-  n.data = value
+func (n *Node) setItem(value int) {
+	n.data = value
 }
+
 //insert at the front
-func (l *List) insertFront(value int) *Node{
-  new := new(Node)
-  new.next = l.head
-  l.head = new
-  l.length++
-  return new
+func (l *List) insertFront(value int) *Node {
+	new := new(Node)
+	new.next = l.head
+	l.head = new
+	l.length++
+	return new
+}
+
+//func to print list contents
+func (l *List) listContents() string {
+	var buffer bytes.Buffer
+	for current := l.head; current != nil; current.next {
+		buffer.WriteString(fmt.Sprintf("%v", current.data))
+		buffer.WriteString(" -> ")
+	}
+	buffer.WriteString("nil")
+	return buffer.String()
 }
