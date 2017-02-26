4a4d5ff5b058bb197629183172680050e2eaee85
Abhishek Gautam <agautam2@buffalo.edu>
2017-02-26 11:51:39 -0800

    list implentation and helper functions

--- LinkedList/GoLang Solutions/linkedlist/linkedlist.go
+++ LinkedList/GoLang Solutions/linkedlist/linkedlist.go
@@ -1,4 +1,36 @@
 package main
 import (
   "fmt"
+  "math/rand"
+  "time"
+  "strings"
+  "strconv"
+  "bytes"
 )
+
+type Node struct {
+  next *Node
+  data int
+}
+
+type List struct {
+  head *Node
+  length int
+}
+
+//func to set next
+func (n *Node) setNext(s *Node){
+  n.next = s
+}
+//func to set data
+func (n *Node) setItem(value int){
+  n.data = value
+}
+//insert at the front
+func (l *List) insertFront(value int) *Node{
+  new := new(Node)
+  new.next = l.head
+  l.head = new
+  l.length++
+  return new
+}
