9e785be3117deec5e3471cfe74e2525cd7cc982f
Abhishek Gautam <agautam2@buffalo.edu>
2017-02-26 12:57:22 -0800

    func randomNumber, randomList #BayAreaCodeMode

--- LinkedList/GoLang Solutions/linkedlist/linkedlist.go
+++ LinkedList/GoLang Solutions/linkedlist/linkedlist.go
@@ -3,6 +3,8 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 type Node struct {
@@ -43,4 +45,27 @@ func (l *List) listContents() string {
 	}
 	buffer.WriteString("nil")
 	return buffer.String()
+}
+
+func (l *List) peakFront() *Node {
+	return l.head
+}
+
+func (l *List) length() int {
+	return l.length
+}
+
+//hey slave! Give me a random integer
+func randomNumberGenerator(min int, max int) int {
+	return min + rand.Intn(max-min)
+}
+
+//hey slave! Give me a list with random values between min and max
+func randomListGenerator(limit, max, min int) *List {
+	rand.Seed(time.Now().UTC().UnixNano())
+	l := new(List)
+	for i := 0; i < limit; i++ {
+		l.insertFront(randomNumberGenerator(min, max))
+	}
+	return l
 }
