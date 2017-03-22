75a9f1bc282ac668f2ca8358dc468c32b79e6744
gautamgitspace <agautam2@buffalo.edu>
2017-03-22 14:39:48 -0700

    basic list structure

---
+++ LinkedList/GoLang Solutions/ctci2_3/ctci_3.go
@@ -0,0 +1,34 @@
+package main
+
+type Node struct {
+	next *Node
+	data int
+}
+type List struct {
+	head *Node
+	size int
+}
+
+func (n *Node) setNext(s *Node) {
+	n.next = s
+}
+func (n *Node) setItem(value int) {
+	n.data = value
+}
+func (l *List) insertFront(value int) *Node {
+	new := new(Node)
+	new.next = l.head
+	l.head = new
+	new.data = value
+	l.size++
+	return new
+}
+func (l *List) peakFront() *Node {
+	return l.head
+}
+func (l *List) sizeOfList() int {
+	return l.size
+}
+func (l *List) sizeDecrement() {
+	l.size--
+}
