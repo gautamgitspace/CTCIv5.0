70340a5dd6905a6e5154531c687260ac3fd4912c
gautamgitspace <agautam2@buffalo.edu>
2017-03-24 12:13:04 -0700

    func removeMiddle and #driver

--- LinkedList/GoLang Solutions/ctci2_3/ctci_3.go
+++ LinkedList/GoLang Solutions/ctci2_3/ctci_3.go
@@ -17,9 +17,11 @@ type List struct {
 func (n *Node) setNext(s *Node) {
 	n.next = s
 }
+
 func (n *Node) setItem(value int) {
 	n.data = value
 }
+
 func (l *List) insertFront(value int) *Node {
 	new := new(Node)
 	new.next = l.head
@@ -28,15 +30,19 @@ func (l *List) insertFront(value int) *Node {
 	l.size++
 	return new
 }
+
 func (l *List) peakFront() *Node {
 	return l.head
 }
+
 func (l *List) sizeOfList() int {
 	return l.size
 }
+
 func (l *List) sizeDecrement() {
 	l.size--
 }
+
 func (l *List) printList() string {
 	var buffer bytes.Buffer
 	for current := l.head; current != nil; current = current.next {
@@ -45,4 +51,33 @@ func (l *List) printList() string {
 	}
 	buffer.WriteString("nil")
 	return buffer.String()
+}
+
+func removeMiddle(l *List, middle *Node) {
+	if middle == nil || l.peakFront() == nil {
+		return
+	}
+	for e := l.peakFront(); e != nil; e = e.next {
+		if e.next == middle {
+			//node hop
+			e.next = e.next.next
+			//de-link middle
+			middle.next = nil
+			middle = nil
+			l.sizeDecrement()
+			return
+		}
+	}
+}
+
+func main() {
+	l := new(List)
+	l.insertFront(1)
+	l.insertFront(2)
+	middle := l.insertFront(3)
+	l.insertFront(4)
+	l.insertFront(5)
+	fmt.Printf("The List: %v\n", l.printList())
+	removeMiddle(l, middle)
+	fmt.Printf("removeMiddle(3) : %v\n", l.printList())
 }
